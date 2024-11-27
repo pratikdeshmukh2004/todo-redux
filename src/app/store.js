d83c52a9761aa1bb98c9d51e457b611fb2f64379
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2024-11-27 17:18:40 +0530

    redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todo/todoSlice";
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = configureStore(
-  { reducer: todoReducer },
-  composerEnhancer()
+  { reducer: todoReducer }
+  // ,composerEnhancer()
 );
